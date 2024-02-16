85dd222c2ea9c4a7544831a4c1f2cf2a2e1792d6
Marko <Marko@DESKTOP-PECANAC>
2024-02-16 20:31:23 +0100

    Add project files.

---
+++ Web aplikacija filmovi prodaja/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_aplikacija_filmovi_prodaja.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
